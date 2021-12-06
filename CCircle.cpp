3d94fac24bc383ee55d24c9ae1442b926ba1f142
OliverKovacs <oliver.kovacs.dev@gmail.com>
2021-12-06 11:23:05 +0100

    Improved circle rendering

--- CCircle.cpp
+++ CCircle.cpp
@@ -1,6 +1,8 @@
 #include "CCircle.h"
 #include <cmath>
 
+#define SQRT_2 1.41421356237
+
 CCircle::CCircle(float x, float y, float radius, const CColor &col, int lt, bool isFilled) :
     CShape2D(col, lt, isFilled), center(x, y), radius(radius) {}
 
@@ -48,13 +50,38 @@ void CCircle::render(const CCanvas &ref) const {
     int height = ref.getHeight();
     std::string **cv = ref.cv;
 
-    for (int x = floor(cX - r * ratio); x < ceil(cX + r * ratio); x++) {
-        for (int y = cY - r; y < cY + r; y++) {
-            if (!(x < width - 1 && y < height && x >= 0 && y >= 0)) continue;
-            double dist = sqrt(pow((cX - x) / ratio, 2) + pow(cY - y, 2));
-            if (dist >= r - 1 / ratio) continue;
-            if (r - 1 <= dist) cv[y][x] = colorString + CCanvas::circleOut + COLOR_RESET;
-            else if (isFilled) cv[y][x] = colorString + CCanvas::fill + COLOR_RESET;
+    std::string stroke = colorString + CCanvas::circleOut + COLOR_RESET;
+    std::string fill = colorString + CCanvas::fill + COLOR_RESET;
+
+    for (int x = 0; x < r * ratio / SQRT_2; x++) {
+        int y = (int)round(sqrt(pow(r, 2) - pow(x / ratio, 2)));
+        if (!(x < width - 1 && y < height && x >= 0 && y >= 0)) continue;
+        cv[cY - y][cX - x] = stroke;
+        cv[cY + y][cX - x] = stroke;
+        cv[cY - y][cX + x] = stroke;
+        cv[cY + y][cX + x] = stroke;
+        if (!isFilled) continue;
+        while (y-- > 0) {
+            cv[cY - y][cX - x] = fill;
+            cv[cY + y][cX - x] = fill;
+            cv[cY - y][cX + x] = fill;
+            cv[cY + y][cX + x] = fill;
+        }
+    }
+
+    for (int y = 0; y < r / SQRT_2; y++) {
+        int x = ratio * (int)round(sqrt(pow(r, 2) - pow(y, 2)));
+        if (!(x < width - 1 && y < height && x >= 0 && y >= 0)) continue;
+        cv[cY - y][cX - x] = stroke;
+        cv[cY + y][cX - x] = stroke;
+        cv[cY - y][cX + x] = stroke;
+        cv[cY + y][cX + x] = stroke;
+        if (!isFilled) continue;
+        while (x-- > 0) {
+            cv[cY - y][cX - x] = fill;
+            cv[cY + y][cX - x] = fill;
+            cv[cY - y][cX + x] = fill;
+            cv[cY + y][cX + x] = fill;
         }
     }
 
